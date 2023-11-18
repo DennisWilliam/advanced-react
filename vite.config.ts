4ff021236e56d6dd67b3a8e2f8f744e8d33801a1
dennisw.dev <dennisw.dev@gmail.com>
2023-11-18 20:49:08 -0300

    Configurando vite e eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  server: { port: 3000, open: "index.html" },
+});
